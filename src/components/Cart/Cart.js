a170a7a2e5d8b15cedb23f7f291ef002d75aa8b1
noormohammad20 <mdsaim204@gmail.com>
2022-04-01 00:57:33 +0600

    Another Recap of React Router Setup

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Cart.css'
+const Cart = () => {
+    return (
+        <div>
+            <h2>This Is Cart</h2>
+        </div>
+    )
+}
+
+export default Cart
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Header.css'
+const Header = () => {
+    return (
+        <div>
+            <h2>Welcome To TShirt Mania!!</h2>
+            <nav>
+                <Link to='home'>Home</Link>
+                <Link to='orderreview'>Order Review</Link>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
